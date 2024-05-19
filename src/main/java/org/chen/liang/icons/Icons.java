9f020aa4c6468b2fac4d76695eb066632856f0f5
陈亮 <chenliang4@tellhow.com>
2024-05-19 22:14:20 +0800

    1、官网弹窗功能学习

---
+++ src/main/java/org/chen/liang/icons/Icons.java
@@ -0,0 +1,11 @@
+package org.chen.liang.icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public interface Icons {
+
+    Icon Sdk_icon = IconLoader.getIcon("/icons/sdk.svg", Icons.class);
+
+}
