1fbf092f044cfb9760b80918bf15c0620e8fd668
陈亮 <chenliang4@tellhow.com>
2024-12-09 18:00:11 +0800

    #IB9SH9: 自动获取Git远程变化

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,8 @@ pluginManagement {
 }
 
 plugins {
+    //If you use Gradle 8.0.2 or higher, you also need to add a toolchain resolver plugin.
+    //  This type of plugin manages which repositories to download a toolchain from
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
