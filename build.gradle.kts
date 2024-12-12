fdadc8a4b73b50f1ffcadffea90786d8b30430d6
西瓜一抹 <1945516094@qq.com>
2024-12-12 15:10:20 +0000

    !2 #IB9SH9: 自动获取Git远程变化

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,47 +1,64 @@
-plugins {
-    id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.9.23"
-    id("org.jetbrains.intellij") version "1.17.2"
-}
-
 group = "me.watermelon"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
+    intellijPlatform {//与dependencies块中的intellijPlatform块结合使用
+        defaultRepositories()
+    }
 }
 
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-    version.set("2023.2.5")
-    type.set("IC") // Target IDE Platform
-
-    plugins.set(listOf(/* Plugin Dependencies */))
+plugins {
+    // A Gradle Core plugin: Provides support for building Java projects. Core plugins must be specified without a version number。
+    // The plugin adds many dependency configurations, such as 'implementation', 'testImplementation' and so on for dependencies,
+    //   https://docs.gradle.org/current/userguide/java_plugin.html#sec:java_plugin_and_dependency_management。
+    id("java")
+    kotlin("jvm") //Kotlin DSL的扩展写法
+    id("org.jetbrains.intellij.platform") version "2.1.0"
 }
 
-tasks {
-    // Set the JVM compatibility versions
-    withType<JavaCompile> {
-        sourceCompatibility = "17"
-        targetCompatibility = "17"
+//https://docs.gradle.org/current/userguide/dependency_configurations.html
+dependencies {
+    //IntelliJ Platform Gradle Plugin enhances the dependencies {} configuration block by applying a nested dependencies.intellijPlatform {} extension.
+    intellijPlatform {
+        intellijIdeaCommunity("2024.2") //Target IDE Platform and Version
+        bundledPlugin("com.intellij.java")
+        bundledPlugin("Git4Idea")
+        instrumentationTools()//if not,execution failed for task ':instrumentCode'.
     }
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
+    implementation(kotlin("stdlib")) //implementation: Dependencies required for both compilation and runtime.
+}
+
+//The IntelliJ Platform Gradle Plugin introduces a top-level intellijPlatform extension
+intellijPlatform {
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild.set("242")
+            untilBuild.set("243.*")//年份后两位 + 大版本号 + 小数点 + 小版本号
+        }
     }
 
-    patchPluginXml {
-        sinceBuild.set("232")
-        untilBuild.set("242.*")
-    }
-
-    signPlugin {
+    signing {
         certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
         privateKey.set(System.getenv("PRIVATE_KEY"))
         password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
     }
 
-    publishPlugin {
+    publishing {
         token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+}
+
+kotlin {
+    //Set the JVM language level used to build the project.
+    jvmToolchain(17)
+}
+
+//register a single task called "hello"
+tasks.register("hello") {
+    group = "custom"
+    description = "A lovely greeting task."
+    doLast {
+        println("Hello world!")
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,17 @@
 pluginManagement {
     repositories {
         mavenCentral()
-        gradlePluginPortal()
+        gradlePluginPortal() //Non-core plugins are available from the Gradle Plugin Portal
     }
+    plugins {
+        kotlin("jvm") version "2.0.21"
+    }
+}
+
+plugins {
+    //If you use Gradle 8.0.2 or higher, you also need to add a toolchain resolver plugin.
+    //  This type of plugin manages which repositories to download a toolchain from
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
 rootProject.name = "IdeaPlugin"
