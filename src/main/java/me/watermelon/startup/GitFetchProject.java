06f5b99aa68ded6eca79a6a6a20b23288c350e67
chen-liang <1945516094@qq.com>
2024-12-14 23:25:39 +0800

    #IB9SH9: 自动获取Git远程变化

--- src/main/java/me/watermelon/startup/GitFetchProject.java
+++ src/main/java/me/watermelon/startup/GitFetchProject.java
@@ -2,6 +2,7 @@ package me.watermelon.startup;
 
 import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import git4idea.GitUtil;
 import git4idea.fetch.GitFetchResult;
@@ -12,12 +13,19 @@ import java.util.Collection;
 
 class GitFetchProject {
 
+    //https://plugins.jetbrains.com/docs/intellij/ide-infrastructure.html#logging
+    public static final Logger LOG = Logger.getInstance(GitFetchProject.class);
+
     static void execute(Project project) {
         Runnable runnable = () -> {
-            GitFetchSupport gitFetchSupport = GitFetchSupport.fetchSupport(project);
-            Collection<GitRepository> repositories = GitUtil.getRepositories(project);
-            GitFetchResult gitFetchResult = gitFetchSupport.fetchAllRemotes(repositories);
-            gitFetchResult.showNotification();
+            try {
+                GitFetchSupport gitFetchSupport = GitFetchSupport.fetchSupport(project);
+                Collection<GitRepository> repositories = GitUtil.getRepositories(project);
+                GitFetchResult gitFetchResult = gitFetchSupport.fetchAllRemotes(repositories);
+                gitFetchResult.showNotification();
+            } catch (Exception e) {//避免异常被展示
+                LOG.error("自动提取仓库远程变动", e);
+            }
         };
 
         Application application = ApplicationManager.getApplication();
