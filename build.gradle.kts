2e44ed53835782fd0250c5fe3eef071c0e268a7e
西瓜一抹 <1945516094@qq.com>
2024-12-15 15:16:58 +0000

    !3 #IB9SH9: 自动获取Git远程变化

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
-group = "me.watermelon"
-version = "1.0"
+group = providers.gradleProperty("pluginGroup").get()
+version = providers.gradleProperty("pluginVersion").get()
 
 repositories {
     mavenCentral()
+    // IntelliJ Platform Gradle Plugin Repositories Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-repositories-extension.html
     intellijPlatform {//与dependencies块中的intellijPlatform块结合使用
         defaultRepositories()
     }
@@ -13,39 +14,40 @@ plugins {
     // The plugin adds many dependency configurations, such as 'implementation', 'testImplementation' and so on for dependencies,
     //   https://docs.gradle.org/current/userguide/java_plugin.html#sec:java_plugin_and_dependency_management。
     id("java")
-    kotlin("jvm") //Kotlin DSL的扩展写法
-    id("org.jetbrains.intellij.platform") version "2.1.0"
+    alias(libs.plugins.kotlin) // Kotlin support；也已使用【kotlin("jvm")】声明(settings.gradle.kts中定义好了版本)，一种Kotlin DSL的扩展写法
+    alias(libs.plugins.intelliJPlatform) // IntelliJ Platform Gradle Plugin；也可使用常规方式【id("org.jetbrains.intellij.platform") version "2.1.0"】声明
 }
 
 //https://docs.gradle.org/current/userguide/dependency_configurations.html
 dependencies {
     //IntelliJ Platform Gradle Plugin enhances the dependencies {} configuration block by applying a nested dependencies.intellijPlatform {} extension.
     intellijPlatform {
-        intellijIdeaCommunity("2024.2") //Target IDE Platform and Version
-        bundledPlugin("com.intellij.java")
-        bundledPlugin("Git4Idea")
+        //Target IDE Platform and Version，only one IntelliJ Platform dependency can be added to the project at a time.
+        // https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html#setting-up-intellij-platform
+        create(providers.gradleProperty("platformType"), providers.gradleProperty("platformVersion"))
+        //intellijIdeaCommunity("2024.2")//也可以使用这个指定
+
+        // Plugin Dependencies. Uses `platformBundledPlugins` property from the gradle.properties file for bundled IntelliJ Platform plugins.
+        bundledPlugins(providers.gradleProperty("platformBundledPlugins").map { it.split(',') })
+
+        // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file for plugin from JetBrains Marketplace.
+        plugins(providers.gradleProperty("platformPlugins").map { it.split(',') })
+
         instrumentationTools()//if not,execution failed for task ':instrumentCode'.
     }
-    implementation(kotlin("stdlib")) //implementation: Dependencies required for both compilation and runtime.
+    //implementation: Dependencies required for both compilation and runtime.
+    //implementation(kotlin("stdlib")) //[intellijIdeaCommunity]依赖中已含有
 }
 
 //The IntelliJ Platform Gradle Plugin introduces a top-level intellijPlatform extension
 intellijPlatform {
     pluginConfiguration {
+        version = providers.gradleProperty("pluginVersion")
+
         ideaVersion {
-            sinceBuild.set("242")
-            untilBuild.set("243.*")//年份后两位 + 大版本号 + 小数点 + 小版本号
+            sinceBuild = providers.gradleProperty("pluginSinceBuild")
+            untilBuild = providers.gradleProperty("pluginUntilBuild")
         }
-    }
-
-    signing {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-    }
-
-    publishing {
-        token.set(System.getenv("PUBLISH_TOKEN"))
     }
 }
 
@@ -54,11 +56,21 @@ kotlin {
     jvmToolchain(17)
 }
 
-//register a single task called "hello"
-tasks.register("hello") {
+//use register() to register a task
+tasks.register("hello") {// 任务名为"hello"
     group = "custom"
     description = "A lovely greeting task."
     doLast {
         println("Hello world!")
     }
 }
+//use named() to configure an existing task registered
+tasks.named<Wrapper>("wrapper") {//对[wrapper]任务进行配置，用于改变使用的gradle版本
+    gradleVersion = providers.gradleProperty("gradleVersion").get()
+}
+//也可以使用如下方式配置已注册的存在的任务，其实就是Kotlin DSL做了包装
+//tasks {
+//    wrapper {
+//        gradleVersion = providers.gradleProperty("gradleVersion").get()
+//    }
+//}
--- src/main/java/me/watermelon/startup/GitFetchProject.java
+++ src/main/java/me/watermelon/startup/GitFetchProject.java
@@ -2,6 +2,7 @@ package me.watermelon.startup;
 
 import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import git4idea.GitUtil;
 import git4idea.fetch.GitFetchResult;
@@ -12,12 +13,16 @@ import java.util.Collection;
 
 class GitFetchProject {
 
+    //https://plugins.jetbrains.com/docs/intellij/ide-infrastructure.html#logging
+    public static final Logger LOG = Logger.getInstance(GitFetchProject.class);
+
     static void execute(Project project) {
         Runnable runnable = () -> {
             GitFetchSupport gitFetchSupport = GitFetchSupport.fetchSupport(project);
             Collection<GitRepository> repositories = GitUtil.getRepositories(project);
             GitFetchResult gitFetchResult = gitFetchSupport.fetchAllRemotes(repositories);
             gitFetchResult.showNotification();
+            LOG.info("自动提取仓库远程变动-成功");
         };
 
         Application application = ApplicationManager.getApplication();
